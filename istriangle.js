f4b1ceabf945385afae46d3fdb4f7eef8b113f29
shubh-37 <shubh.arya37@gmail.com>
2022-10-04 01:44:53 +0530

    wrote the logic of the app

---
+++ istriangle.js
@@ -0,0 +1,27 @@
+var inputAngle = document.querySelectorAll(".angle-input");
+var checkBtn = document.querySelector("#check-btn");
+var output = document.querySelector("#output");
+
+function sumOfTriangle(a,b,c){
+    return a+b+c;
+}
+
+function isTriangle(){
+    //validating the input before doing the processing
+    if(inputAngle[0].value&&inputAngle[1].value&&inputAngle[2].value){
+        //processing the input
+        var sum = sumOfTriangle(Number(inputAngle[0].value),Number(inputAngle[1].value),Number(inputAngle[2].value))
+        
+        if(sum === 180){
+            //rendering the output
+            output.innerText = "This is a triangle!"
+        }else{
+            output.innerText = "Oops! not a triangle"
+        }
+    }else{
+        //error handler
+        output.innerText = "Please enter all the inputs!!";
+    }       
+}
+
+checkBtn.addEventListener("click", isTriangle);
