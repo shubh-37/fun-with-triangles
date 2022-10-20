6b958b886881a27b107221e273d0b3be8434647c
shubh-37 <shubh.arya37@gmail.com>
2022-10-20 13:06:18 +0530

    feat: error handling for negative values

--- istriangle.js
+++ istriangle.js
@@ -9,15 +9,20 @@ function sumOfTriangle(a,b,c){
 function isTriangle(){
     //validating the input before doing the processing
     if(inputAngle[0].value&&inputAngle[1].value&&inputAngle[2].value){
-        //processing the input
+        if(inputAngle[0].value > 0 && inputAngle[1].value > 0 && inputAngle[2].value > 0){
+            //processing the input
         var sum = sumOfTriangle(Number(inputAngle[0].value),Number(inputAngle[1].value),Number(inputAngle[2].value))
         
         if(sum === 180){
             //rendering the output
-            output.innerText = "This is a triangle!"
+            output.innerText = "This is a triangle!";
         }else{
-            output.innerText = "Oops! not a triangle"
+            output.innerText = "Oops! not a triangle";
         }
+        }else{
+            //error handler
+            output.innerText = "Please enter positive values to proceed!";
+        }      
     }else{
         //error handler
         output.innerText = "Please enter all the inputs!!";
