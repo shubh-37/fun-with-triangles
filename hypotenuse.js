491f618ab881f936d7f7bc162c8e3818c81fda1c
shubh-37 <shubh.arya37@gmail.com>
2022-10-20 12:57:22 +0530

    feat: error handling for negative values

--- hypotenuse.js
+++ hypotenuse.js
@@ -9,15 +9,18 @@ function sumOfSquares(a,b){
 function calcHypotenuse(){
     //input validators
     if(inputSide[0].value&&inputSide[1].value){
-        
-        //processing the input
+        if(inputSide[0].value > 0 && inputSide[1].value>0){
+            //processing the input
         var side1 = inputSide[0].value;
         var side2 = inputSide[1].value;
         var sumOfSquare = sumOfSquares(side1,side2);
         var hypotenuse = Math.sqrt(sumOfSquare);
 
         //rendering the output
-        output.innerText = "The hypotenuse is " + hypotenuse;
+        output.innerText = "The hypotenuse is " + hypotenuse;    
+        }else{
+            output.innerText = "Please enter positive values to proceed!"
+        }
     }else{
         //error handler
         output.innerText = "Please enter both the inputs to proceed!!!"
